07223c187eabc582ae6f73e147d9e202e484e349
ProgIqor <118454389+ProgIqor@users.noreply.github.com>
2023-01-13 09:37:28 -0300

    Delete Email-Teste.js

--- Email-Teste.js
+++
@@ -1,7 +0,0 @@
-const nodemailer = require('nodemailer');
-
-const transport = nodemailer.createTransport({    host: 'smtp.gmail.com',port: 465,secure: true, auth: {user: 'chargerbattery33@gmail.com',pass: 'msjnhxxpjyknumme', }});
-
-transport.sendMail({from: 'Battery System <chargerbattery33@gmail.com>',to: 'chargerbattery33@gmail.com',subject: 'Alert - Batt#1 Finished',text: 'Olá Email, à bateria está carregada conforme configuração',})
-.then(() => console.log('Email enviado com sucesso!'))
-.catch((err) => console.log('Erro ao enviar email: ', err));
